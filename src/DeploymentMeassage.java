1bc5f526c1bd28045c060944e0dbe5a4e1390bda
Alifa Colaco <Joycy.Colaco@sabre.com>
2017-05-09 13:01:20 +0530

    Initial commit

---
+++ src/DeploymentMeassage.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SG0227814 on 5/9/2017.
+ */
+public class DeploymentMeassage {
+    public static void main(String[] args)
+    {
+        System.out.println("This is Deployment Meassage");
+    }
+}
